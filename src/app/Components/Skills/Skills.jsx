9483d6be79ee0b7a267fc527dd1c8d850b02b760
Ujjawal-04 <145132249+Ujjawal-04@users.noreply.github.com>
2024-10-08 18:04:40 +0530

    Update Skills.jsx

--- src/app/Components/Skills/Skills.jsx
+++ src/app/Components/Skills/Skills.jsx
@@ -8,28 +8,11 @@ const Skills = () => {
     <div className={styles.skill}>
     <div className={styles.language}>
      <h2>Languages :-</h2>
-     <br />
-      • Python
-      <br />
-      • C
-      <br />
-      • C++
-      <br />
-      • Java
+[languages]
     </div>
     <div className={styles.web}>
     <h2>Web Development :- </h2>
-      <br />
-      • HTML &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;• Next
-      <br />
-      • CSS &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;• Mern Stack
-      <br />
-      • Javascript&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;• Mongo DB
-      <br />
-      • Tailwind CSS&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; • SQL 
-      <br />
-      • React&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;• Firebase
-      <br />
+     [skills]
     </div>
     </div>
     </section>
